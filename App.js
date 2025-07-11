d7e2804ac3595180f4d3a1a5003598ddb254877b
preethi-hue <preethibalu6711@gmail.com>
2025-07-11 19:21:04 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
-import './App.css';
-import{BrowserRouter as Router,Routes,Route}from'react-router-dom';
-import Home from './pages/Home';
-import Create from'./pages/create';
-import Next from'./pages/Next';
-import Password1 from './pages/Password1';
-import Login1 from'./pages/Login1';
-import Thank1 from'./pages/Thank1';
-
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route}from 'react-router-dom';
+import Home1 from './pages/Home1';
+import Login from'./pages/Login';
+import Register from'./pages/Register';
+import'./App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/create" element ={<Create/>}/>
-        <Route path="/Next" element ={<Next/>}/>
-       <Route path="/password1" element={<Password1/>}/>
-       <Route path="/Login1" element={<Login1/>}/>
-       <Route path="/thank1" element={<Thank1/>}/>
-       
+      <Route path="/" element={<Home1 />}/>
+      <Route path="/Login" element={<Login />}/>
+      <Route path="/Register" element={<Register />}/>
       </Routes>
-    </Router>
+      </Router>
   );
 }
+
 export default App;
